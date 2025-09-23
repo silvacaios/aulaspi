4617959edc7dec477ffe726df0115bf792bdedeb
Caio Victor Dos Santos Silva <20211204010055@ifrn.local>
2025-09-23 10:55:20 -0300

    Colocando os arquivos iniciais da minha aplicação

---
+++ src/test/java/ifrn/PI/eventos/EventosApplicationTests.java
@@ -0,0 +1,13 @@
+package ifrn.PI.eventos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
