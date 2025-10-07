b62f9f506a1b3363537685b9cf612fa19ad96734
Caio Victor Dos Santos Silva <20211204010055@ifrn.local>
2025-10-07 10:58:17 -0300

    criando um controller de Evento

---
+++ src/main/java/ifrn/PI/eventos/controllers/EventodController.java
@@ -0,0 +1,13 @@
+package ifrn.PI.eventos.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class EventodController {
+
+	@RequestMapping(" /Evento/form")
+	public String form() {
+		return "formEvento";
+	}
+}
